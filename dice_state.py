3ef1c996ce3f092456f5ce7fa79c1b4a3187aa8a
Zhiyuan Li <zl7904@g.rit.edu>
2017-10-10 14:07:52 -0400

    dice state

---
+++ dice_state.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class dice_state():
+	def __init__(self,location,top,north,east):
+		self.location=location
+		self.top=
+		self.north=
+		self.east=
+	def get_children(self,grid):
+		
+		
+	def 
