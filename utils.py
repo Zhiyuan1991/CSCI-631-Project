a253df0daf5b488c5ebb50c825e2c91df0c1395d
bjk4704 <bjk4704@mail.rit.edu>
2017-11-30 22:02:41 -0500

    Added functionality for converting CIFAR into LAB space

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cPickle
 import numpy as np
+from skimage import color
 
 def read_cifar_batch_data(filename):
 	with open(filename, 'rb') as fo:
@@ -18,4 +19,21 @@ def read_cifar_batch_data(filename):
 	one_hot[np.arange(l),labels]=1
 	return data,one_hot
 
+def read_cifar_batch_data_lab(filename):
+	with open(filename, 'rb') as fo:
+		dict = cPickle.load(fo)
+	data=dict['data']
+	labels=dict['labels']
 
+	#reset image structure
+	data=data.reshape([32, 32, 3])
+	data=color.rgb2lab(data)
+	data=data.reshape([-1, 3, 32, 32])
+	data=data.transpose([0, 2, 3, 1])
+	data=data.reshape([-1,3*32*32])
+
+	l=len(labels)
+	class_num=100
+	one_hot=np.zeros((l,class_num))
+	one_hot[np.arange(l),labels]=1
+	return data,one_hot
